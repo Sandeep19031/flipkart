b1fa62db3d745c2dc91cb8e799e68f3418769b9e
Sandeep Kumar <sandeep.kumar.19031@iitgoa.ac.in>
2021-12-18 14:41:34 +0530

    nothing

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,12 +4,59 @@ import Navbar from '../Navbar/Navbar'
 import NavCard from '../NavCard/NavCard'
 import './Home.css'
 
+import CartLogo1 from './cart-logo.png';
+
+const HomeProductCard = ({productName, productPrice, productBrand,productImgSrc}) => {
+    return(
+        <div className="homeProductCard">
+            <div className="homeProductCard-img">
+                <img src={productImgSrc} alt={productName} width='40%'/>
+            </div>
+            <div className="homeProductCard-description">
+                <p>{productName}</p>
+                <p>₹{productPrice}</p>
+                <p>{productBrand}</p>
+            </div>
+        </div>
+    );
+}
+const ProductsCard = ({title,}) => {
+    return (
+        <div className="productsCard">
+            <div className="productsCard-left">
+                <div className="productsCard-left-up">
+                    <p>{title}</p>
+                    <div className="button">
+                        <p>VIEW ALL</p>
+                    </div>
+                </div>
+                
+                <div className="productsCard-left-down">
+                    <img src={CartLogo1} alt="cart" />
+                </div>
+            </div>
+            <div className="productsCard-right">
+                <HomeProductCard
+                productName="SAMSUNG Galaxy Z Flip3 5G"
+                productPrice="84,999"
+                productBrand="SAMSUNG"
+                productImgSrc="https://rukminim1.flixcart.com/image/416/416/ksnjp8w0/mobile/f/k/y/galaxy-z-flip3-5g-sm-f711bzkainu-samsung-original-imag662axn2xmght.jpeg?q=70"
+                />
+            </div>
+        </div>
+    );
+}
 function Home() {
     return (
         <div>
             <Navbar />
             <NavCard />
-            <div>Body</div>
+            <div className="body">
+                <ProductsCard 
+                title="Top Offers on Mobiles"
+                />
+            </div>
+            
             <Bottom  />
         </div>
     )
