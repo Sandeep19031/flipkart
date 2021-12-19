6d29628838802216f2a53a9c0d3cd809c59552cf
adityajain23 <aditya.jain.19031@iitgoa.ac.in>
2021-12-19 23:34:44 +0530

    done

--- src/pages/AllProducts/AllProducts.js
+++ src/pages/AllProducts/AllProducts.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./AllProducts.css";
 import MobileCards from "../cardsTemplates/mobilesCard";
+import { RangeStepInput } from "react-range-step-input";
+import { useEffect, useState } from "react";
 
 function AllProducts() {
+  const [PriceValue, setPriceValue] = useState(0);
+  const [IsChecked, setIsChecked] = useState(new Array(7).fill(false));
+
+  const handleOnChange = (position) => {
+    const updatedCheckedState = IsChecked.map((item, index) =>
+      index === position ? !item : item
+    );
+    setIsChecked(updatedCheckedState);
+    console.log(updatedCheckedState);
+  };
+
   return (
     <div className="allProducts">
       <div className="filters-container">
@@ -11,40 +24,74 @@ function AllProducts() {
         </div>
         <div className="price">
           <p className="price-heading">PRICE</p>
-          <div className="price-slider"></div>
-          <div className="price-range"></div>
+          <div className="price-slider">
+            <RangeStepInput
+              min={0}
+              max={30000}
+              value={PriceValue}
+              step={2000}
+              onChange={(e) => {
+                const newVal = e.target.value;
+                setPriceValue(newVal);
+                console.log(newVal);
+              }}
+            />
+            <span className="price-value">{PriceValue}</span>
+          </div>
         </div>
         <div className="brand">
           <p className="brand-heading">BRAND</p>
           <label className="container">
-            <input className="checkbox" type="checkbox" />
+            <input
+              className="checkbox"
+              type="checkbox"
+              checked={IsChecked[0]}
+              onChange={() => handleOnChange(0)}
+            />
             Mi
           </label>
           <br />
 
           <label className="container">
-            <input className="checkbox" type="checkbox" />
+            <input
+              className="checkbox"
+              type="checkbox"
+              checked={IsChecked[1]}
+              onChange={() => handleOnChange(1)}
+            />
             Realme
           </label>
         </div>
         <div className="rating">
           <p className="rating-heading">CUSTOMER RATING</p>
           <label className="container">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              checked={IsChecked[2]}
+              onChange={() => handleOnChange(2)}
+            />
             4★ & above
             <span className="checkmark"></span>
           </label>
           <br></br>
 
           <label className="container">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              checked={IsChecked[3]}
+              onChange={() => handleOnChange(3)}
+            />
             3★ & above
             <span className="checkmark"></span>
           </label>
           <br></br>
 
           <label className="container">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              checked={IsChecked[4]}
+              onChange={() => handleOnChange(4)}
+            />
             2★ & above
             <span className="checkmark"></span>
           </label>
@@ -52,14 +99,22 @@ function AllProducts() {
         <div className="discount">
           <p className="discount-heading">DISCOUNT</p>
           <label className="container">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              checked={IsChecked[5]}
+              onChange={() => handleOnChange(5)}
+            />
             10% or more
             <span className="checkmark"></span>
           </label>
           <br></br>
 
           <label className="container">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              checked={IsChecked[6]}
+              onChange={() => handleOnChange(6)}
+            />
             5% or more
             <span className="checkmark"></span>
           </label>
--- src/pages/cardsTemplates/mobilesCard.js
+++ src/pages/cardsTemplates/mobilesCard.js
@@ -1,197 +1,9 @@
 import React from "react";
 import "./mobilesCard.css";
-// import ListingAll from "../data/mobiles.mjs";
 import axios from "axios";
-
-const getData = async () => {
-  var dataL;
-  await axios
-    .get("http://localhost:9000/mobiles")
-    .then((response) => {
-      const data = response.data;
-      dataL = data;
-      console.log("Data has been Received!!");
-    })
-    .catch(() => {
-      alert("Error!!");
-    });
-  return dataL;
-};
-
-let dataList = [
-  {
-    title: "Redmi 9 (Sporty Orange, 64 GB)  (4 GB RAM)",
-    link: "https://www.linkpicture.com/q/Redmi-9-Sporty-Orange-64GB.jpeg",
-    original_price: 10760,
-    updated_price: 10090,
-    discount: 6,
-    highlights: [
-      "4 GB RAM | 64 GB ROM",
-      "16.59 cm (6.53 inch) HD+ Display",
-      "13MP + 2MP | 5MP Front Camera",
-      "5000 mAh Battery",
-      "MediaTek Helio G35 Processor",
-    ],
-    warranty:
-      "1 year manufacturer warranty for device and 6 months manufacturer warranty for in-box accessories including batteries from the date of purchase",
-  },
-  {
-    title: "Mi 11X (Celestial Silver, 128 GB)  (8 GB RAM)",
-    link: "https://www.linkpicture.com/q/Mi-11X-Celestial-Silver-128GB.jpeg",
-    original_price: 26190,
-    updated_price: 26190,
-    discount: 0,
-    highlights: [
-      "8 GB RAM | 128 GB ROM",
-      "16.94 cm (6.67 inch) Full HD+ Display",
-      "48MP + 8MP + 5MP | 20MP Front Camera",
-      "4520 mAh Battery",
-      "Qualcomm Snapdragon 870 Processor",
-    ],
-    warranty:
-      "1 Year Manufacturer Warranty for Device and 6 Months Manufacturer Warranty for In-box Accessories",
-  },
-  {
-    title: "MI 11X 5G (COSMIC BLACK, 128 GB)  (6 GB RAM)",
-    link: "https://www.linkpicture.com/q/MI-11X-5G-Cosmic-Black-128GB.jpeg",
-    original_price: 26300,
-    updated_price: 26300,
-    discount: 0,
-    highlights: [
-      "6 GB RAM | 128 GB ROM",
-      "16.94 cm (6.67 inch) Display",
-      "48MP Rear Camera",
-      "4520 mAh Battery",
-    ],
-    warranty: "1 YEAR",
-  },
-  {
-    title: "MI 11X 5G (LUNAR WHITE, 128 GB)  (6 GB RAM)",
-    link: "https://www.linkpicture.com/q/MI-11X-5G-Lunar-White-128GB.jpeg",
-    original_price: 28995,
-    updated_price: 28995,
-    discount: 0,
-    highlights: [
-      "6 GB RAM | 128 GB ROM",
-      "16.94 cm (6.67 inch) Display",
-      "48MP Rear Camera",
-      "4520 mAh Battery",
-    ],
-    warranty: "1 Year",
-  },
-  {
-    title: "Redmi 9 (Carbon Black, 64 GB)  (4 GB RAM)",
-    link: "https://www.linkpicture.com/q/Redmi-9-Carbon-Black-64GB.jpeg",
-    original_price: 10380,
-    updated_price: 10380,
-    discount: 0,
-    highlights: [
-      "4 GB RAM | 64 GB ROM",
-      "16.59 cm (6.53 inch) HD+ Display",
-      "13MP + 2MP | 5MP Front Camera",
-      "5000 mAh Battery",
-      "MediaTek Helio G35 Processor",
-    ],
-    warranty:
-      "1 year manufacturer warranty for device and 6 months manufacturer warranty for in-box accessories including batteries from the date of purchase",
-  },
-  {
-    title: "Redmi K20 (Carbon Black, 128 GB)  (6 GB RAM)#JustHere",
-    link: "https://www.linkpicture.com/q/Redmi-K20-Carbon-Black-128GB.jpeg",
-    original_price: 20985,
-    updated_price: 20985,
-    discount: 0,
-    highlights: [
-      "6 GB RAM | 128 GB ROM",
-      "16.23 cm (6.39 inch) Full HD+ Display",
-      "48MP + 13MP + 8MP | 20MP Front Camera",
-      "4000 mAh Li-polymer Battery",
-      "Qualcomm Snapdragon 730 Processor",
-    ],
-    warranty:
-      "Brand Warranty of 1 Year Available for Mobile and 6 Months for Accessories",
-  },
-  {
-    title: "REDMI NOTE 10 LITE (Glacier White, 128 GB)  (4 GB RAM)",
-    link: "https://www.linkpicture.com/q/Redmi-Note-10-lite-Glacier-White-128GB.jpeg",
-    original_price: 15975,
-    updated_price: 15975,
-    discount: 0,
-    highlights: [
-      "4 GB RAM | 128 GB ROM",
-      "16.94 cm (6.67 inch) Display",
-      "48MP Rear Camera",
-      "5020 mAh Battery",
-    ],
-    warranty: "1 Year",
-  },
-  {
-    title: "Redmi 9A (Nature Green, 32 GB)  (2 GB RAM)",
-    link: "https://www.linkpicture.com/q/Redmi-9A-Nature-Green-32GB.jpeg",
-    original_price: 7888,
-    updated_price: 7888,
-    discount: 0,
-    highlights: [
-      "2 GB RAM | 32 GB ROM",
-      "16.59 cm (6.53 inch) Full HD+ Display",
-      "13MP Rear Camera",
-      "5000 mAh Battery",
-    ],
-    warranty: "1 Year Manufacturer Warranty",
-  },
-  {
-    title: "Redmi 9A (SeaBlue, 32 GB)  (2 GB RAM)",
-    link: "https://www.linkpicture.com/q/Redmi-9A-SeaBlue-32GB.jpeg",
-    original_price: 7902,
-    updated_price: 7902,
-    discount: 0,
-    highlights: [
-      "2 GB RAM | 32 GB ROM",
-      "16.59 cm (6.53 inch) Full HD+ Display",
-      "13MP Rear Camera",
-      "5000 mAh Battery",
-    ],
-    warranty: "1 Year Manufacturer Warranty",
-  },
-  {
-    title: "Xiaomi 11Lite NE (Diamond Dazzle, 128 GB)  (8 GB RAM)",
-    link: "https://www.linkpicture.com/q/Xiaomi-11Lite-NE-Diamond-Dazzle-128GB.jpeg",
-    original_price: 28999,
-    updated_price: 28999,
-    discount: 0,
-    highlights: [
-      "8 GB RAM | 128 GB ROM | Expandable Upto 1 TB",
-      "16.64 cm (6.55 inch) Full HD+ Display",
-      "64MP + 8MP + 5MP | 20MP Front Camera",
-      "4250 mAh Battery",
-      "Qualcomm Snapdragon 778G Processor",
-    ],
-    warranty:
-      "Brand Warranty of 1 Year Available for Mobile and 6 Months for Accessories",
-  },
-];
-
-// let dataList = ListingAll();
-
-// let data = {
-//   title: "Redmi 9 (Sporty Orange, 64 GB)  (4 GB RAM)",
-//   link: "https://www.linkpicture.com/q/Redmi-9-Sporty-Orange-64GB.jpeg",
-//   original_price: 10760,
-//   updated_price: 10090,
-//   discount: 6,
-//   highlights: [
-//     "4 GB RAM | 64 GB ROM",
-//     "16.59 cm (6.53 inch) HD+ Display",
-//     "13MP + 2MP | 5MP Front Camera",
-//     "5000 mAh Battery",
-//     "MediaTek Helio G35 Processor",
-//   ],
-//   warranty:
-//     "1 year manufacturer warranty for device and 6 months manufacturer warranty for in-box accessories including batteries from the date of purchase",
-// };
+import { useEffect, useState } from "react";
 
 function MobileCard({ data }) {
-  // console.log(params);
   return (
     <div className="mobileCard">
       <div className="card-image">
@@ -241,12 +53,15 @@ function MobileCard({ data }) {
 }
 
 function MobileCards() {
-  const dataL = getData();
-  // dataL.resolve().then(() => console.log(2));
-  // console.log("hello");
+  const [DataList, setDataList] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:9000/mobiles").then((res) => {
+      setDataList(res.data);
+    });
+  }, []);
   return (
     <div>
-      {dataList.map((data) => (
+      {DataList.map((data) => (
         <MobileCard data={data} />
       ))}
     </div>
