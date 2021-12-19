63f3d2abbb0c3b47832e52846669c0c7b83f981b
adityajain23 <aditya.jain.19031@iitgoa.ac.in>
2021-12-19 13:16:45 +0530

    done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,92 +1,102 @@
-import React from 'react'
-import Bottom from '../Bottom/Bottom'
-import Navbar from '../Navbar/Navbar'
-import NavCard from '../NavCard/NavCard'
-import './Home.css'
+import React from "react";
+import Bottom from "../Bottom/Bottom";
+import Navbar from "../Navbar/Navbar";
+import NavCard from "../NavCard/NavCard";
+import "./Home.css";
 
-import CartLogo1 from './cart-logo.png';
-import AllProducts from '../AllProducts/AllProducts'
+import CartLogo1 from "./cart-logo.png";
+import AllProducts from "../AllProducts/AllProducts";
 
-
-
-const HomeProductCard = ({productName, productPrice, productBrand,productImgSrc}) => {
-    return(
-        <div className="homeProductCard">
-            <div className="homeProductCard-img">
-                <img src={productImgSrc} alt={productName}  />
-            </div>
-            <div className="homeProductCard-description">
-                <p className="homeProductCard-description-productName">{productName}</p>
-                <p className="homeProductCard-description-productPrice">₹{productPrice}</p>
-                <p className="homeProductCard-description-productBrand">{productBrand}</p>
-            </div>
+const HomeProductCard = ({
+  productName,
+  productPrice,
+  productBrand,
+  productImgSrc,
+}) => {
+  return (
+    <div className="homeProductCard">
+      <div className="homeProductCard-img">
+        <img src={productImgSrc} alt={productName} />
+      </div>
+      <div className="homeProductCard-description">
+        <p className="homeProductCard-description-productName">{productName}</p>
+        <p className="homeProductCard-description-productPrice">
+          ₹{productPrice}
+        </p>
+        <p className="homeProductCard-description-productBrand">
+          {productBrand}
+        </p>
+      </div>
+    </div>
+  );
+};
+const ProductsCard = ({ title, list }) => {
+  return (
+    <div className="productsCard">
+      <div className="productsCard-left">
+        <div className="productsCard-left-up">
+          <p>{title}</p>
+          <div className="button">VIEW ALL</div>
         </div>
-    );
-}
-const ProductsCard = ({title,list}) => {
-    return (
-        <div className="productsCard">
-            <div className="productsCard-left">
-                <div className="productsCard-left-up">
-                    <p>{title}</p>
-                    <div className="button">
-                        VIEW ALL
-                    </div>
-                </div>
-                
-                <div className="productsCard-left-down">
-                    <img src={CartLogo1} alt="cart" height='100%'/>
-                </div>
-            </div>
-            <div className="productsCard-right">
-                {list.map((val,_) => {
-                    
-                    return (<HomeProductCard
-                    productName= {val.productName}
-                    productPrice= {val.productPrice}
-                    productBrand= {val.productBrand}
-                    productImgSrc = {val.productImgSrc}
-                    />);
-                })}
-            </div>
+
+        <div className="productsCard-left-down">
+          <img src={CartLogo1} alt="cart" height="100%" />
         </div>
-    );
-}
+      </div>
+      <div className="productsCard-right">
+        {list.map((val, _) => {
+          return (
+            <HomeProductCard
+              productName={val.productName}
+              productPrice={val.productPrice}
+              productBrand={val.productBrand}
+              productImgSrc={val.productImgSrc}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 function Home() {
-    const HomeProductCard_Mobiles = [
-        {
-            productName: "SAMSUNG Galaxy Z Flip3 5G",
-            productPrice: "84,999",
-            productBrand: "SAMSUNG",
-            productImgSrc:"https://rukminim1.flixcart.com/image/416/416/ksnjp8w0/mobile/f/k/y/galaxy-z-flip3-5g-sm-f711bzkainu-samsung-original-imag662axn2xmght.jpeg?q=70"
-        },
-        {
-            productName: "realme 8s 5G",
-            productPrice: "17,999",
-            productBrand: "realme",
-            productImgSrc: "https://rukminim1.flixcart.com/image/416/416/ktbu6q80/mobile/f/s/e/8s-5g-rmx3381-realme-original-imag6zhgra57dtqe.jpeg?q=70"
-        }
-    ];
-    const HomeProductCard_Fashions = [
-        {
-            productName: "Women Floral Print Rayon A-line Kurta ",
-            productPrice: "657",
-            productBrand: "METRO-FASHION ",
-            productImgSrc:"https://rukminim1.flixcart.com/image/880/1056/kkec4280/kurta/r/z/r/s-kr452a-metro-fashion-original-imafzr5zyytvgvrp.jpeg?q=50"
-        },
-        {
-            productName: "Women Printed Rayon Straight Kurta",
-            productPrice: "399",
-            productBrand: "LIBAS",
-            productImgSrc: "https://rukminim1.flixcart.com/image/880/1056/k7gikcw0/kurta/q/3/x/xl-8072mj-blue-libas-original-imafpz7gqjhf8thg.jpeg?q=50"
-        }
-    ];
-    return (
-        <div>
-            <Navbar />
-            <NavCard />
-            <div className="body">
-                {/* <ProductsCard 
+  const HomeProductCard_Mobiles = [
+    {
+      productName: "SAMSUNG Galaxy Z Flip3 5G",
+      productPrice: "84,999",
+      productBrand: "SAMSUNG",
+      productImgSrc:
+        "https://rukminim1.flixcart.com/image/416/416/ksnjp8w0/mobile/f/k/y/galaxy-z-flip3-5g-sm-f711bzkainu-samsung-original-imag662axn2xmght.jpeg?q=70",
+    },
+    {
+      productName: "realme 8s 5G",
+      productPrice: "17,999",
+      productBrand: "realme",
+      productImgSrc:
+        "https://rukminim1.flixcart.com/image/416/416/ktbu6q80/mobile/f/s/e/8s-5g-rmx3381-realme-original-imag6zhgra57dtqe.jpeg?q=70",
+    },
+  ];
+  const HomeProductCard_Fashions = [
+    {
+      productName: "Women Floral Print Rayon A-line Kurta ",
+      productPrice: "657",
+      productBrand: "METRO-FASHION ",
+      productImgSrc:
+        "https://rukminim1.flixcart.com/image/880/1056/kkec4280/kurta/r/z/r/s-kr452a-metro-fashion-original-imafzr5zyytvgvrp.jpeg?q=50",
+    },
+    {
+      productName: "Women Printed Rayon Straight Kurta",
+      productPrice: "399",
+      productBrand: "LIBAS",
+      productImgSrc:
+        "https://rukminim1.flixcart.com/image/880/1056/k7gikcw0/kurta/q/3/x/xl-8072mj-blue-libas-original-imafpz7gqjhf8thg.jpeg?q=50",
+    },
+  ];
+  return (
+    <div>
+      <Navbar />
+      <NavCard />
+      <div className="body">
+        {/* <ProductsCard 
                 title="Top Offers on Mobiles"
                 list={HomeProductCard_Mobiles}
                 />
@@ -94,12 +104,12 @@ function Home() {
                 title="Today's Fashion Deals"
                 list={HomeProductCard_Fashions}
                 /> */}
-                <AllProducts />
-            </div>
-            
-            <Bottom  />
-        </div>
-    )
+        <AllProducts />
+      </div>
+
+      <Bottom />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
---
+++ src/pages/cardsTemplates/mobilesCard.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "./mobilesCard.css";
+
+let data = {
+  title: "Redmi 9 (Sporty Orange, 64 GB)  (4 GB RAM)",
+  link: "https://www.linkpicture.com/q/Redmi-9-Sporty-Orange-64GB.jpeg",
+  original_price: 10760,
+  updated_price: 10090,
+  discount: 6,
+  highlights: [
+    "4 GB RAM | 64 GB ROM",
+    "16.59 cm (6.53 inch) HD+ Display",
+    "13MP + 2MP | 5MP Front Camera",
+    "5000 mAh Battery",
+    "MediaTek Helio G35 Processor",
+  ],
+  warranty:
+    "1 year manufacturer warranty for device and 6 months manufacturer warranty for in-box accessories including batteries from the date of purchase",
+};
+
+function MobileCard() {
+  return (
+    <div className="mobileCard">
+      <div className="card-image">
+        <img src={data.link} alt="blank" />
+      </div>
+      <div className="card-desc">
+        <p className="card-title">{data.title}</p>
+        <div className="rate-block">
+          <p>
+            <span className="rate-star">4.5★</span>23,232 Rating & 734 Reviews
+          </p>
+        </div>
+        <div>
+          <ul className="specsList">
+            {data.highlights.map((item, i) => (
+              <li className="listItem" key={i}>
+                {item}
+              </li>
+            ))}
+            <li className="listItem">{data.warranty}</li>
+          </ul>
+        </div>
+      </div>
+      <div className="card-price">
+        <p>
+          &#x20b9;
+          {data.updated_price.toLocaleString()}
+          <span>
+            <img
+              className="logo"
+              src="https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png"
+              alt="fAssured"
+            />
+          </span>
+        </p>
+        <p className="discount">
+          <span>
+            <del className="original_price">
+              &#x20b9;{data.original_price.toLocaleString()}
+            </del>
+          </span>
+          {data.discount}% off
+        </p>
+      </div>
+    </div>
+  );
+}
+
+MobileCard();
+
+export default MobileCard;
